33f0e0b097b5be909499d3f0174475b11dfa34c3
cesarwizeline <cesar.romero@wizeline.com>
2021-10-11 10:48:40 -0500

    add provider to index

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,10 @@ import * as React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import { App } from "./components/App";
-
+import { Provider } from 'react-redux';
+import { store } from './stores/store';
 render(
-    <App />,
-    document.getElementById("root")
-);
+    <Provider store={store} >
+        <App />
+    </Provider>,
+    document.getElementById("root"));
