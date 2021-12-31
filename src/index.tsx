4c72248b4d038eda37432067beddea0bdf7ca32b
cesarwizeline <cesar.romero@wizeline.com>
2021-12-31 10:44:31 -0600

    fix error building the app

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import * as React from "react";
+import React from "react";
 import { render } from "react-dom";
 import "./index.css";
 import { App } from "./components/App";
 import { Provider } from 'react-redux';
 import { store } from './stores/store';
 render(
-    <Provider store={store} >
-        <App />
-    </Provider>,
-    document.getElementById("root"));
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById("root"));
